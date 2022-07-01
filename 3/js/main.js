cda0b953aabc185c19fe439546d87ad8279954d0
Keks <keks@htmlacademy.ru>
2022-07-01 19:51:18 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,19 +1,20 @@
 const getRandomNumber = function (numberMin, numberMax) {
-    if (numberMin < 0) {
-        return 0;
-    }
-    if (numberMin >= numberMax) {
-        return numberMax;
-    }
-    //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-    numberMin = Math.ceil(numberMin);
-    numberMax = Math.floor(numberMax);
-    return Math.floor(Math.random() * (numberMax - numberMin + 1)) + numberMin;
-}
+  if (numberMin < 0) {
+    return 0;
+  }
+  if (numberMin >= numberMax) {
+    return numberMax;
+  }
+  //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  numberMin = Math.ceil(numberMin);
+  numberMax = Math.floor(numberMax);
+  console.log('Зашли в функцию');
+  return Math.floor(Math.random() * (numberMax - numberMin + 1)) + numberMin;
+};
 
 const checkMaxLength = function (stringCheck, maxSize) {
-    return (String.length(stringCheck) <= maxSize) ? true : false;
-}
+  return stringCheck.length <= maxSize;
+};
 
 getRandomNumber(1, 5);
-checkMaxLength("123456", 7);
+checkMaxLength('123456', 7);
